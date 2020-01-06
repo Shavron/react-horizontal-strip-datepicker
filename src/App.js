2f3eacb9d336ae388542da0eb8dadfc7be32cf6a
Shavron <ashu.9x@gmail.com>
2020-01-07 00:13:12 +0530

    horizontal datepicker with react functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import  './App.css'
+import ReactHorizontalDatePicker from './ReactHorizontalDatePicker';
 
 function App() {
+ 
+  const onSelectedDay = (d) =>{
+      console.log(d)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ReactHorizontalDatePicker selectedDay={onSelectedDay} enableScroll={true} enableDays={90}/> 
   );
 }
 
